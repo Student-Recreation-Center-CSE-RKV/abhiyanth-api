165e31f5a07d583677201d75b20a0cbcaa1914e2
Nagarjuna0033 <nagarjunabanda4@gmail.com>
2024-12-07 20:14:07 +0530

    added .env file to gitignore

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,19 +1,18 @@
+/* eslint-disable linebreak-style */
 module.exports = {
   env: {
     es6: true,
     node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    // eslint-disable-next-line object-curly-spacing, quote-props
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
   },
   overrides: [
     {
